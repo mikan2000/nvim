aa0801b5771ab3020e0952cd194c16be017d26e4
mikan <lsmxinjing@gmail.com>
2024-07-08 16:00:28 +0800

    git

--- init.lua
+++ init.lua
@@ -12,6 +12,7 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 		})
 	end,
 })
+
 -- keybindings
 local opt = { noremap = true, silent = true }
 vim.keymap.set("n", "H", "0", opt)
@@ -24,6 +25,11 @@ vim.keymap.set("n", "<C-j>", "<C-w>j", opt)
 vim.keymap.set("n", "<C-k>", "<C-w>k", opt)
 vim.keymap.set("n", "<Leader>v", "<C-w>v", opt)
 vim.keymap.set("n", "<Leader>s", "<C-w>s", opt)
+vim.keymap.set("n", "<leader><leader>", ":noh<CR>", opt)
+
+-- https://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
+vim.keymap.set("n", "j", [[v:count ? 'j' : 'gj']], { noremap = true, expr = true })
+vim.keymap.set("n", "k", [[v:count ? 'k' : 'gk']], { noremap = true, expr = true })
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -43,4 +49,38 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
-require("lazy").setup({})
+require("lazy").setup({
+	{
+		"RRethy/nvim-base16",
+		lazy = true,
+	},
+	{
+		cmd = "Telescope",
+		keys = {
+			{ "<leader>p", ":Telescope find_files<CR>", desc = "find files" },
+			{ "<leader>P", ":Telescope live_grep<CR>", desc = "grep file" },
+			{ "<leader>rs", ":Telescope resume<CR>", desc = "resume" },
+			{ "<leader>q", ":Telescope oldfiles<CR>", desc = "oldfiles" },
+		},
+    		'nvim-telescope/telescope.nvim',
+		tag = '0.1.8',
+		-- or                              , branch = '0.1.x',
+      		dependencies = { 'nvim-lua/plenary.nvim' }
+    	},
+	{
+	    event = "VeryLazy",
+	    "williamboman/mason.nvim",
+	    build = ":MasonUpdate",
+	    config = function()
+		require('mason').setup()
+	    end
+	},
+	{
+		event = "VeryLazy",
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
+})
+vim.cmd.colorscheme("base16-tender")
